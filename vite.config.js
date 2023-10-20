cac90fa69ae776782f9f211ca4e854a0c7b763f9
Ej Angelo Dalman <41765719+YongJo@users.noreply.github.com>
2023-10-20 23:48:54 +0800

    Initial Working

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig({
   build: {
     lib: {
       entry: resolve(__dirname, "src/index.jsx"),
-      name: "umd-library",
+      name: "Serino",
 
       fileName: "index",
     },
